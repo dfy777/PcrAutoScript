2fa230ac7ab07a1ed7008fa153c72b463f206f54
Mirai <wr568367887@gmail.com>
2020-10-14 22:36:10 +0800

    增加简单交互

---
+++ myexcept.py
@@ -0,0 +1,20 @@
+class UnfindSimulatorException(Exception):
+    def __init__(self, ErrorInfo):
+        self.errorinfo = ErrorInfo
+
+    def __str__(self):
+        return self.errorinfo
+
+class UnfindAdbException(Exception):
+    def __init__(self, ErrorInfo):
+        self.errorinfo = ErrorInfo
+    
+    def __str__(self):
+        return self.errorinfo
+
+class ConnectException(Exception):
+    def __init__(self, ErrorInfo):
+        self.errorinfo = ErrorInfo
+    
+    def __str__(self):
+        return self.errorinfo
